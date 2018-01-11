cef51e0fc0359e387c79e91f9a05264bce60cf93
Avery Yip <averyyip@ymail.com>
2018-01-11 12:52:56 -0500

    Post routing completed

--- app/views.py
+++ app/views.py
@@ -1,9 +1,18 @@
 from flask import render_template, jsonify, redirect, flash, request
 from app import app
+import json
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/dialogflow', methods=['POST'])
+def dialogflow_webhook():
+	json_req = request.get_json(silent=True, force=True)
+
+	print("Request:")
+	print(json.dumps(json_req, indent=4))
+	return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
