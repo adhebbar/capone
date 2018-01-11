b8b734b3b1c08cb281172ee22a3e9cedd3faf79e
Marc Harvey <marcharvey1470@gmail.com>
2018-01-11 15:47:00 -0600

    added time duration support for dialogflow bot

--- app/webhook.py
+++ app/webhook.py
@@ -1,34 +1,49 @@
 import json
+import datetime
+
 
 ### Main import ###
 def respond(json_dict):
 	"""Identifies the intent within a json as a dict and returns the corresponding action"""
 	intent = get_intent(json_dict)
+	timeFrame = get_timeFrame(json_dict)
+
 
 	if intent == 'Default Welcome Intent':
 		return welcome()
 	elif intent == "Balance":
 		return balance()
 	elif intent == "Habits":
-		return habits()
+		return habits(timeFrame)
 	elif intent == "Predictions":
-		return predictions()
+		return predictions(timeFrame)
+	elif intent == "Recommendations":
+		return recommendations(timeFrame)
+
 
 ### Intents ###
 def welcome():
 	return make_fulfillment("Hello")
 
+
 def balance():
 	balance = 1000
 	return make_fulfillment("Your balance is $" + str(balance))
 
-def habits():
+
+def habits(timeFrame):
 	habits = 'test works for habits'
 	return make_fulfillment(habits)
 
-def predictions():
+
+def predictions(timeFrame):
 	predictions = 'test works for predictions'
 	return make_fulfillment(predictions)
+
+
+def recommendations(timeFrame):
+	recommendations = 'test works for recommendations'
+	return make_fulfillment(recommendations)
 
 ### Tools ###
 def make_fulfillment(text):
@@ -36,5 +51,15 @@ def make_fulfillment(text):
 	response_dict = {'fulfillmentText':text}
 	return json.dumps(response_dict)
 
+
 def get_intent(json_dict):
 	return json_dict['queryResult']['intent']['displayName']
+
+
+def get_timeFrame(json_dict):
+	startDate = json_dict['queryResult']['parameters']['startDate'][0:10]
+	endDate = json_dict['queryResult']['parameters']['endDate'][0:10]
+	realStart = datetime.datetime.strptime(startDate, '%Y-%m-%d')
+	realEnd = datetime.datetime.strptime(endDate, '%Y-%m-%d')
+	timeFrame = realEnd - realStart
+	return int(timeFrame.days)
